d6e2f9d8dbb703c61cb79f4dbacf4ab80fe61761
Mehmetusakk <33814009+Mehmetusakk@users.noreply.github.com>
2022-09-20 00:11:51 +0300

    Add files via upload
    Ders 6 notlarıdır.

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 package my_java_project;
 
 public class Main {
-
+	
+	public static void main (String[] args ){
+		
+		
+		int Sayi = 10;
+		String Tablename = "TabloA";
+		Boolean Sonuc = true;
+		double myDouble = 9.58;
+		float myFloat = 50.5f;	
+		
+	
+	}	
+	
 }
